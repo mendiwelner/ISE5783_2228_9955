4434a92ccc54d953a5cf914cd6d41549e6e62c4b
mendysegal490@gmail.com <mendysegal490@gmail.com>
2023-03-13 22:17:03 +0200

    Tests succeeded!

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -5,7 +5,7 @@ import static java.lang.Math.*;
 public class Point {
 	protected Double3 xyz;
 	
-	Point(double x,double y,double z) {
+	public Point(double x,double y,double z) {
 		
 		xyz = new Double3(x,y,z);
 	}
