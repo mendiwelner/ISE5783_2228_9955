392a3cd9e8d116fc28fc8bf5fec3f21c53c5c315
mendiwelner <meniwell@gmail.com>
2023-04-04 00:08:57 +0300

    tests

---
+++ src/unittests/geometries/SphereTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package unittests.geometries;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author meniw
+ *
+ */
+class SphereTest {
+
+	/**
+	 * Test method for {@link geometries.Sphere#Sphere(double, primitives.Point)}.
+	 */
+	@Test
+	void testSphere() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link geometries.Sphere#getCenter()}.
+	 */
+	@Test
+	void testGetCenter() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/unittests/primitives/VectorTest.java
+++ src/unittests/primitives/VectorTest.java
@@ -3,31 +3,73 @@
  */
 package unittests.primitives;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 import primitives.*;
 import org.junit.jupiter.api.Test;
 
+import geometries.Polygon;
+
 /**
- * @author mendy 
+ * @author mendy
  *
  */
-class VectorTest {
+
+public class VectorTest {
+
+	/** Test method for {@link geometries.Polygon#Polygon(primitives.Point...)}. */
+	@Test
+	public void testConstructor() {
+		// ============ Equivalence Partitions Tests ==============
+
+		// TC01: Correct vector
+		assertDoesNotThrow(() -> new Vector(1, 1, 1), //
+				"Failed constructing a correct polygon");
+		// TC02: Correct vector
+		assertDoesNotThrow(() -> new Vector(-1, -1, -1), //
+				"Failed constructing a correct polygon");
+
+		// =============== Boundary Values Tests ==================
+		// TC03: zero vector, wrong!
+		assertThrows(IllegalArgumentException.class, //
+				() -> new Vector(0, 0, 0), //
+				"ERROR: zero vector throws wrong exception");
+	}
 
 	/**
 	 * Test method for {@link primitives.Vector#add(primitives.Vector)}.
 	 */
 	@Test
 	void testAddVector() {
-		Vector v1 = new Vector(1,2,3);
-		// what about subtract method in Vector class? 
-		assertEquals(new Vector(-1,-2,-3),v1.add(new Vector(-2,-4,-6))
-				,"(1,2,3) + (-2,-4,-6) must be (-1,-2,-3)");
-		assertEquals(new Vector(3,6,9),v1.subtract(new Vector(-2,-4,-6))
-				,"(1,2,3) - (-2,-4,-6) must be (3,6,9)");
-		assertThrows(IllegalArgumentException.class,()-> v1.add(new Vector(-1,-2,-3))
-				,"This should be exeption of vector(0,0,0)");
-		assertThrows(IllegalArgumentException.class,()-> v1.subtract(v1)
-				,"This should be exeption of vector(0,0,0)");		
+		Vector v1 = new Vector(1, 2, 3);
+		// ============ Equivalence Partitions Tests ==============
+		// TC04: Correct adding vectors
+		assertEquals(new Vector(2, 4, 6), v1.add(new Vector(1, 2, 3)), "(1,2,3) + (-2,-4,-6) must be (-1,-2,-3)");
+		// TC05: Correct adding vectors
+		assertEquals(new Vector(-1, -2, -3), v1.add(new Vector(-2, -4, -6)), "(1,2,3) + (-2,-4,-6) must be (-1,-2,-3)");
+
+		// =============== Boundary Values Tests ==================
+		// TC06: Getting zero vector
+		assertThrows(IllegalArgumentException.class, () -> v1.add(new Vector(-1, -2, -3)),
+				"This should be exeption of vector(0,0,0)");
+	}
+
+	/**
+	 * Test method for {@link primitives.Vector#subtract(primitives.Vector)}.
+	 */
+	@Test
+	void testSubtractVector() {
+		Vector v1 = new Vector(1, 2, 3);
+		// ============ Equivalence Partitions Tests ==============
+		// TC07: Correct subtracting vectors
+		assertEquals(new Vector(2, 4, 6), v1.subtract(new Vector(-1, -2, -3)), "(1,2,3) - (-1,-2,-3) must be (2,4,6)");
+		// TC08: Correct subtracting vectors
+		assertEquals(new Vector(-1, -2, -3), v1.subtract(new Vector(2, 4, 6)), "(1,2,3) - (2,4,6) must be (-1,-2,-3)");
+
+		// =============== Boundary Values Tests ==================
+		// TC09: Getting zero vector
+		assertThrows(IllegalArgumentException.class, () -> v1.subtract(new Vector(1, 2, 3)),
+				"This should be exeption of vector(0,0,0)");
 	}
 
 	/**
@@ -35,10 +77,21 @@ class VectorTest {
 	 */
 	@Test
 	void testScale() {
-		Vector v1 = new Vector(1,2,3);
-		assertEquals(new Vector(2,4,6),v1.scale(2),"2*(1,2,3) must be (2,4,6)");
-		assertThrows(IllegalArgumentException.class,()-> v1.scale(0)
-				,"0*(1,2,3) should be exeption of vector(0,0,0)");
+		Vector v1 = new Vector(1, 2, 3);
+		// ============ Equivalence Partitions Tests ==============
+		// TC10: Correct scaling
+		assertEquals(new Vector(2, 4, 6), v1.scale(2), "2*(1,2,3) must be (2,4,6)");
+		// TC11: Correct scaling
+		assertEquals(new Vector(1, 2, 3), v1.scale(1), "1*(1,2,3) must be (1,2,3)");
+		// TC12: Correct scaling
+		assertEquals(new Vector(-1, -2, -3), v1.scale(-1), "-1*(1,2,3) must be (-1,-2,-3)");
+		// TC13: Correct scaling
+		assertEquals(new Vector(-2, -4, -6), v1.scale(-2), "-2*(1,2,3) must be (-2,-4,-6)");
+		
+		// =============== Boundary Values Tests ==================
+		// TC14: Getting zero vector
+		assertThrows(IllegalArgumentException.class, () -> v1.scale(0),
+				"0*(1,2,3) should be exeption of vector(0,0,0)");
 	}
 
 	/**
