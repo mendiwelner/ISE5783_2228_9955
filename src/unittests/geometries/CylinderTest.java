527f936c8c4df828f1ee3a90089045da6d6893fd
mendiwelner <meniwell@gmail.com>
2023-05-02 04:23:44 +0300

    .

--- src/unittests/geometries/CylinderTest.java
+++ src/unittests/geometries/CylinderTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package unittests.geometries;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,52 +5,58 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import geometries.Cylinder;
-import primitives.Point;
-import primitives.Ray;
-import primitives.Vector;
+import primitives.*;
 
 /**
- * @author Mendy&Mendy 
- * Mendy Segal. 211769955. Mendysegal490@gmail.com
- * Mendy Welner 209272228. mendiwell@gmail.com
+ * Testing Cylinder
+ * 
+ * @author Mendy Welner 209272228. mendiwell@gmail.com
+ *         Mendy Segal. 211769955. Mendysegal490@gmail.com 
  *
  */
 class CylinderTest {
 
 	/**
-	 * Test method for {@link geometries.Cylinder#Cylinder(double, primitives.Ray, double)}.
+	 * Test method for
+	 * {@link geometries.Cylinder#Cylinder(double, primitives.Ray, double)}.
 	 */
 	@Test
 	void testConstructor() {
-		Ray axisRay = new Ray(new Point(1,0,0),new Vector(0,0,1));
+		Ray axisRay = new Ray(new Point(1, 0, 0), new Vector(0, 0, 1));
 		// ============ Equivalence Partitions Tests ==============
 		// TC01: Correct Cylinder with radius 5 and height 10
-		assertDoesNotThrow(() -> new Cylinder(5,axisRay,10) ,"Failed constructing a correct cylinder");
-		
+		assertDoesNotThrow(() -> new Cylinder(5, axisRay, 10), "Failed constructing a correct cylinder");
+
 		// TC02: Correct Cylinder with radius 2.5 and height 8.5
-		assertDoesNotThrow(() -> new Cylinder(2.5,axisRay,8.5) ,"Failed constructing a correct cylinder");
-		
+		assertDoesNotThrow(() -> new Cylinder(2.5, axisRay, 8.5), "Failed constructing a correct cylinder");
+
 		// TC03: Wrong Cylinder with radius -5 and height 10
-		assertThrows(IllegalArgumentException.class ,() -> new Cylinder(-5,axisRay,10) 
-				,"Failed constructing a correct cylinder");
-		
+		assertThrows(IllegalArgumentException.class, () -> new Cylinder(-5, axisRay, 10),
+				"Failed constructing a correct cylinder");
+
 		// TC04: Wrong Cylinder with radius 5 and height -10
-		assertThrows(IllegalArgumentException.class ,() -> new Cylinder(5,axisRay,-10) 
-				,"Failed constructing a correct cylinder");
-		
+		assertThrows(IllegalArgumentException.class, () -> new Cylinder(5, axisRay, -10),
+				"Failed constructing a correct cylinder");
+
 		// TC05: Wrong Cylinder with radius 5 and height 0
-		assertThrows(IllegalArgumentException.class ,() -> new Cylinder(5,axisRay,0) 
-				,"Failed constructing a correct cylinder");
+		assertThrows(IllegalArgumentException.class, () -> new Cylinder(5, axisRay, 0),
+				"Failed constructing a correct cylinder");
 	}
-	
+
 	/**
 	 * Test method for {@link geometries.Cylinder#getNormal(primitives.Point)}.
 	 */
 	@Test
 	void testGetNormal() {
+<<<<<<< HEAD
 		Ray axisRay = new Ray(new Point(0,0,0),new Vector(0,0,1));
 		Cylinder cyl = new Cylinder(1 , axisRay , 3);
+=======
+		Ray axisRay = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1));
+		Cylinder cyl = new Cylinder(1, axisRay, 3);
+>>>>>>> branch 'master' of https://github.com/mendiwelner/ISE5783_2228_9955.git
 		// ============ Equivalence Partitions Tests ==============
+<<<<<<< HEAD
 		//TC01: Point is between the bases
 		assertEquals(new Vector(1,0,0),cyl.getNormal(new Point(1,0,1)));
 		
@@ -63,18 +66,43 @@ class CylinderTest {
 		//TC03: Point is at the bottom base
 		assertEquals(new Vector(0,0,-1),cyl.getNormal(new Point(1,0,0)));
 		
+=======
+		// TC01: Point is between the bases
+		assertEquals(new Vector(1, 0, 0), cyl.getNormal(new Point(1, 0, 1)));
+
+		// TC02: Point is at the top base
+		assertEquals(new Vector(0, 0, 1), cyl.getNormal(new Point(1, 0, 3)));
+
+		// TC03: Point is at the bottom base
+		assertEquals(new Vector(0, 0, -1), cyl.getNormal(new Point(0, 1, 0)));
+
+>>>>>>> branch 'master' of https://github.com/mendiwelner/ISE5783_2228_9955.git
 		// =============== Boundary Values Tests ==================
+<<<<<<< HEAD
 		//TC04: Point is at the top center base
 		assertEquals(new Vector(0,0,1),cyl.getNormal(new Point(0,0,3)));
 		
 		//TC05: Point is at the bottom center base
 		assertEquals(new Vector(0,0,-1),cyl.getNormal(new Point(0,0,0)));
+=======
+		// TC04: Point is at the top center base
+		assertEquals(new Vector(0, 0, 1), cyl.getNormal(new Point(0, 0, 3)));
+
+		// TC05: Point is at the bottom center base
+		assertEquals(new Vector(0, 0, -1), cyl.getNormal(new Point(0, 0, 0)));
+
+>>>>>>> branch 'master' of https://github.com/mendiwelner/ISE5783_2228_9955.git
 		
 		// Point is at the edge top base
-		//assertEquals(new Vector(0,0,1),cyl.getNormal(new Point(1,0,3)));
-		
+		// assertEquals(new Vector(0,0,1),cyl.getNormal(new Point(1,0,3)));
+
 		// Point is at the edge bottom base
-		//assertEquals(new Vector(0,0,-1),cyl.getNormal(new Point(0,0,0)));
+		// assertEquals(new Vector(0,0,-1),cyl.getNormal(new Point(0,0,0)));
 	}
+	
+	void testFindIntsersections() {
+		fail("Not yet implemented");
+	}
+	
+	
 }
-		
--- src/unittests/geometries/TubeTest.java
+++ src/unittests/geometries/TubeTest.java
@@ -1,22 +1,25 @@
-/**
- * 
- */
 package unittests.geometries;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import primitives.*;
+<<<<<<< HEAD
 import geometries.Cylinder;
 import geometries.Sphere;
+=======
+>>>>>>> branch 'master' of https://github.com/mendiwelner/ISE5783_2228_9955.git
 import geometries.Tube;
+<<<<<<< HEAD
 
 import static primitives.Util.isZero;
+=======
+>>>>>>> branch 'master' of https://github.com/mendiwelner/ISE5783_2228_9955.git
 
 /**
- * @author Mendy&Mendy
- * Mendy Segal. 211769955. Mendysegal490@gmail.com
- * Mendy Welner 209272228. mendiwell@gmail.com
+ * Testing Tube
+ * 
+ * @author Mendy Welner 209272228. mendiwell@gmail.com
+ *         Mendy Segal. 211769955. Mendysegal490@gmail.com 
  */
 class TubeTest {
 
@@ -26,18 +29,18 @@ class TubeTest {
 	@Test
 	void testConstructor() {
 		// ============ Equivalence Partitions Tests ==============
-		Ray axisRay = new Ray(new Point(1,0,0),new Vector(0,0,1));
+		Ray axisRay = new Ray(new Point(1, 0, 0), new Vector(0, 0, 1));
 		// TC01: Correct Tube with radius 5
-		assertDoesNotThrow(() -> new Tube(5,axisRay) ,"Failed constructing a correct Tube");
-		
+		assertDoesNotThrow(() -> new Tube(5, axisRay), "Failed constructing a correct Tube");
+
 		// TC02: Correct Tube with radius 2.5
-		assertDoesNotThrow(() -> new Tube(2.5,axisRay) ,"Failed constructing a correct Tube");
-		
+		assertDoesNotThrow(() -> new Tube(2.5, axisRay), "Failed constructing a correct Tube");
+
 		// TC03: Wrong Tube with radius -3
-		assertThrows(IllegalArgumentException.class, () -> new Tube(-3,axisRay) ,"The radius must be greater then 0");
-		
+		assertThrows(IllegalArgumentException.class, () -> new Tube(-3, axisRay), "The radius must be greater then 0");
+
 		// TC04: Wrong Tube with radius 0
-		assertThrows(IllegalArgumentException.class , ()-> new Tube(0,axisRay),"The radius cannot be 0");
+		assertThrows(IllegalArgumentException.class, () -> new Tube(0, axisRay), "The radius cannot be 0");
 	}
 
 	/**
@@ -45,17 +48,21 @@ class TubeTest {
 	 */
 	@Test
 	void testGetNormal() {
-        Ray ray = new Ray(new Point(0, 2, 2), new Vector(1, 0, 0));
-        Tube tube = new Tube(5,ray);
+		Ray ray = new Ray(new Point(0, 2, 2), new Vector(1, 0, 0));
+		Tube tube = new Tube(5, ray);
 
-        // ============ Equivalence Partitions Tests ==============
-        // TC01: Wrong normal calculation (in case the point is not across the ray.p0)
-        assertEquals(new Vector(0 ,-Math.sqrt(1/2d) ,-Math.sqrt(1/2d)), tube.getNormal(new Point(1,1,1))
-                ,"getNormal does not work correctly");
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: Wrong normal calculation (in case the point is not across the ray.p0)
+		assertEquals(new Vector(0, -Math.sqrt(1 / 2d), -Math.sqrt(1 / 2d)), tube.getNormal(new Point(1, 1, 1)),
+				"getNormal does not work correctly");
 
-        // =============== Boundary Values Tests ==================
-        // TC02: Wrong normal calculation (in case the point is across the ray.p0)
-        assertEquals(new Vector(0 ,-Math.sqrt(1/2d) ,-Math.sqrt(1/2d)), tube.getNormal(new Point(0, 1, 1))
-                ,"getNormal does not work correctly (Boundary test)");
+		// =============== Boundary Values Tests ==================
+		// TC02: Wrong normal calculation (in case the point is across the ray.p0)
+		assertEquals(new Vector(0, -Math.sqrt(1 / 2d), -Math.sqrt(1 / 2d)), tube.getNormal(new Point(0, 1, 1)),
+				"getNormal does not work correctly (Boundary test)");
+	}
+	
+	void testFindIntersections() {
+		fail("Not yet implemented");
 	}
 }
