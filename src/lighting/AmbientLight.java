4764db5662627dc3e1c217e1a95b4b79cc160e5b
mendysegal490@gmail.com <mendysegal490@gmail.com>
2023-05-23 19:25:07 +0300

    y

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -13,7 +13,7 @@ import primitives.*;
 
 
 public class AmbientLight {
-	private final static AmbientLight NONE = new AmbientLight(Color.BLACK,Double3.ZERO);
+	public final static AmbientLight NONE = new AmbientLight(Color.BLACK,Double3.ZERO);
 	private final Color intensity;
 	
 	AmbientLight(Color color,Double3 kA){
--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -2,11 +2,21 @@
  * 
  */
 package scene;
+import primitives.*;
+import geometries.Geometries;
+import lighting.*;
 
 /**
  * @author mendy
  *
  */
 public class Scene {
+	
+	public String name;
+	public Color background = Color.BLACK;
+	public AmbientLight ambientLight = AmbientLight.NONE;
+	public Geometries geometries = new Geometries();
+	
+	Scene()
 
 }
