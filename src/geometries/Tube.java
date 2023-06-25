74d3ff320530ce78bb15a54b919dbc378511346c
mendiwelner <meniwell@gmail.com>
2023-06-26 01:54:14 +0300

    סיום שלב 7

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,8 +1,13 @@
 package geometries;
 
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
 import java.util.List;
 
-import primitives.*;
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
 
 /**
  * The class Tube extends the RadialGeometry class and allows us to represent a
@@ -11,7 +16,7 @@ import primitives.*;
  * @author Mendy Welner 209272228. mendiwell@gmail.com Mendy Segal.
  *         211769955.Mendysegal490@gmail.com
  */
-public class Tube extends RadialGeometry { 
+public class Tube extends RadialGeometry {
 
 	/** axis ray of the tube - represents the whole axis line */
 	protected final Ray axisRay;
@@ -36,6 +41,64 @@ public class Tube extends RadialGeometry {
 
 	@Override
 	public List<GeoPoint> findGeoIntersectionsHelper(Ray ray) {
+		Vector v = ray.getDir();
+		Vector v0 = axisRay.getDir();
+
+		// Calculating temp1 = v - v0 * (v,v0)
+		Vector temp1 = v;
+		double vv0 = v.dotProduct(v0);
+		if (!isZero(vv0)) {
+			Vector v0vv0 = v0.scale(vv0);
+			if (v.equals(v0vv0)) {
+				return null;
+			}
+			temp1 = v.subtract(v0vv0);
+		}
+
+		// Calculating temp2 = dp - v0 * (dp,v0) where dp = p0 - p
+		double temp1DotTemp2 = 0;
+		double squaredTemp2 = 0;
+		if (!ray.getP0().equals(axisRay.getP0())) {
+			Vector dp = ray.getP0().subtract(axisRay.getP0());
+			Vector temp2 = dp;
+			double dpv0 = dp.dotProduct(v0);
+			if (isZero(dpv0)) {
+				temp1DotTemp2 = temp1.dotProduct(temp2);
+				squaredTemp2 = temp2.lengthSquared();
+			} else {
+				Vector v0dpv0 = v0.scale(dpv0);
+				if (!dp.equals(v0dpv0)) {
+					temp2 = dp.subtract(v0dpv0);
+					temp1DotTemp2 = temp1.dotProduct(temp2);
+					squaredTemp2 = temp2.lengthSquared();
+				}
+			}
+		}
+
+		// Getting the quadratic equation: at^2 +bt + c = 0
+		double a = temp1.lengthSquared();
+		double b = 2 * temp1DotTemp2;
+		double c = alignZero(squaredTemp2 - radius * radius);
+
+		double squaredDelta = alignZero(b * b - 4 * a * c);
+		if (squaredDelta <= 0) {
+			return null;
+		}
+
+		double delta = Math.sqrt(squaredDelta);
+		double t1 = alignZero((-b + delta) / (2 * a));
+		double t2 = alignZero((-b - delta) / (2 * a));
+
+		if (t1 > 0 && t2 > 0) {
+			return List.of(new GeoPoint(this, ray.getPoint(t1)), new GeoPoint(this, ray.getPoint(t2)));
+		}
+		if (t1 > 0) {
+			return List.of(new GeoPoint(this, ray.getPoint(t1)));
+		}
+		if (t2 > 0) {
+			return List.of(new GeoPoint(this, ray.getPoint(t2)));
+		}
+
 		return null;
 	}
 
---
+++ src/unittests/renderer/ourTests.java
@@ -0,0 +1,57 @@
+package unittests.renderer;
+
+import org.junit.jupiter.api.Test;
+
+import geometries.Sphere;
+import geometries.Triangle;
+import geometries.Tube;
+
+import lighting.SpotLight;
+import primitives.*;
+import renderer.*;
+import scene.Scene;
+
+/**
+ * test class for our scene
+ * 
+ * @author Mendy Welner 209272228. mendiwell@gmail.com Mendy Segal.
+ *         211769955.Mendysegal490@gmail.com
+ */
+public class ourTests {
+	private Scene scene = new Scene("Test scene");
+
+	/** Produce a picture of a sphere lighted by a spot light */
+	@Test
+	public void table() {
+		Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setVPSize(150, 150).setVPDistance(1000);
+
+		scene.geometries.add( //
+				new Triangle(new Point(-50, -50, -100), new Point(50, -50, -100), new Point(0, -30, -50)) //
+						.setEmission(new Color(150, 10, 10)) //
+						.setMaterial(new Material().setShininess(100).setKr(0.8).setKt(1)),
+				new Triangle(new Point(0, -30, -50), new Point(50, -50, -100), new Point(-10, 50, -15)) //
+						.setEmission(new Color(10, 150, 10)) //
+						.setMaterial(new Material().setShininess(100).setKr(0.8).setKt(1)),
+				new Triangle(new Point(0, -30, -50), new Point(-50, -50, -100), new Point(-10, 50, -15)) //
+						.setEmission(new Color(10, 10, 150)) //
+						.setMaterial(new Material().setShininess(100).setKr(0.8).setKt(1)),
+				new Triangle(new Point(-50, -50, -100), new Point(50, -50, -100), new Point(-10, 50, -15)) //
+						.setEmission(new Color(100, 100, 20)) //
+						.setMaterial(new Material().setKr(0.2).setKt(0)),
+				new Sphere(10d, new Point(0, -20, -65)).setEmission(new Color(200, 20, 200)) //
+						.setMaterial(new Material().setKd(0.3).setKs(0.3).setShininess(50)),
+				new Tube(4, new Ray(new Point(-10, 0, -55), new Vector(1, 0, 0))).setEmission(new Color(150, 20, 220)) //
+						.setMaterial(new Material().setKd(0.3).setKs(0.3).setShininess(50)),
+				new Tube(8, new Ray(new Point(-10, 20, -80), new Vector(1, 0, 0))).setEmission(new Color(100, 50, 250)) //
+						.setMaterial(new Material().setKd(0.3).setKs(0.3).setShininess(100)));
+		scene.lights.add( //
+				new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
+						.setKl(0.0004).setKq(0.0000006));
+
+		camera.setImageWriter(new ImageWriter("table", 500, 500)) //
+				.setRayTracer(new RayTracerBasic(scene)) //
+				.renderImage() //
+				.writeToImage();
+	}
+}
